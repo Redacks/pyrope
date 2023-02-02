448d19ae550d92123eb144475b313cfccfa33b20
Redacks <redacks@web.de>
2023-02-02 18:40:08 +0100

    Initial Commit

---
+++ pyrope-gui/Components/MainWindow/MenuBar.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace pyropeGUI.Components.MainWindow
+{
+    public partial class MenuBar : UserControl
+{
+	public MenuBar()
+	{
+		InitializeComponent();
+	}
+}
+}
---
+++ pyrope-gui/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace pyropeGUI
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
